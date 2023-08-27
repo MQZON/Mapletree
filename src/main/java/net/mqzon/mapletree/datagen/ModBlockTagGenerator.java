358715b13c7cdce1e5c74faaf9ee29e44c1bc307
MQZON <mxwllsky@gmail.com>
2023-08-26 21:16:17 -0700

    Add tool mining tags

--- src/main/java/net/mqzon/mapletree/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/mqzon/mapletree/datagen/ModBlockTagGenerator.java
@@ -5,6 +5,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.common.Mod;
 import net.mqzon.mapletree.Mapletree;
 import net.mqzon.mapletree.block.ModBlocks;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +19,26 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(BlockTags.MINEABLE_WITH_HOE).add(
+                ModBlocks.MAPLE_LEAVES.get()
+        );
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(
+                ModBlocks.MAPLE_LOG.get(),
+                ModBlocks.MAPLE_WOOD.get(),
+                ModBlocks.STRIPPED_MAPLE_LOG.get(),
+                ModBlocks.STRIPPED_MAPLE_WOOD.get(),
+                ModBlocks.MAPLE_PLANKS.get(),
+                ModBlocks.MAPLE_STAIRS.get(),
+                ModBlocks.MAPLE_SLAB.get(),
+                ModBlocks.MAPLE_FENCE.get(),
+                ModBlocks.MAPLE_FENCE_GATE.get(),
+                ModBlocks.MAPLE_DOOR.get(),
+                ModBlocks.MAPLE_TRAPDOOR.get(),
+                ModBlocks.MAPLE_PRESSURE_PLATE.get(),
+                ModBlocks.MAPLE_BUTTON.get()
+        );
+
         this.tag(BlockTags.FENCES)
                 .add(ModBlocks.MAPLE_FENCE.get());
         this.tag(BlockTags.FENCE_GATES)
