63af70e6cef9c584fe4a1292523f45c3a5a28a42
MQZON <mxwllsky@gmail.com>
2023-09-09 16:20:25 -0700

    Remove unused imports

--- src/main/java/net/mqzon/mapletree/event/ModEvents.java
+++ src/main/java/net/mqzon/mapletree/event/ModEvents.java
@@ -1,14 +1,10 @@
 package net.mqzon.mapletree.event;
 
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.common.BasicItemListing;
-import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
