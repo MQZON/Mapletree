1ca5b13c734793ee0fe7c6dec72261f00024d5d8
MQZON <mxwllsky@gmail.com>
2024-12-01 12:24:08 -0800

    Initial fabric commit

---
+++ src/main/java/net/mqzon/mapletree/MapletreeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mqzon.mapletree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MapletreeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
