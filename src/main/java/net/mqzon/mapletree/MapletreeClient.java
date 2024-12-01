b82b3f4091171e902ab4b45b5a093f5fc537cd49
MQZON <mxwllsky@gmail.com>
2024-12-01 12:59:35 -0800

    Add client and basic mod config

---
+++ src/main/java/net/mqzon/mapletree/MapletreeClient.java
@@ -0,0 +1,10 @@
+package net.mqzon.mapletree;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapletreeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
