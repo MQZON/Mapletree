67fa49649e80e339acf58328c7e94e5a8a116ceb
MQZON <mxwllsky@gmail.com>
2024-12-01 13:38:50 -0800

    Add first item (sap bucket)

--- src/main/java/net/mqzon/mapletree/Mapletree.java
+++ src/main/java/net/mqzon/mapletree/Mapletree.java
@@ -2,6 +2,7 @@ package net.mqzon.mapletree;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mqzon.mapletree.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Mapletree implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/mqzon/mapletree/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.mqzon.mapletree.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.mqzon.mapletree.Mapletree;
+
+public class ModItems {
+
+    public static Item SAP_BUCKET = registerItem("sap_bucket", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Mapletree.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Mapletree.LOGGER.info("Registering Mod Items for" + Mapletree.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SAP_BUCKET);
+        });
+    }
+}
