ea77f18f5308277ef444773c3175643ca1b7e8df
MQZON <mxwllsky@gmail.com>
2024-12-01 17:33:04 -0800

    Add first block (maple planks)

--- src/main/java/net/mqzon/mapletree/Mapletree.java
+++ src/main/java/net/mqzon/mapletree/Mapletree.java
@@ -2,6 +2,7 @@ package net.mqzon.mapletree;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mqzon.mapletree.block.ModBlocks;
 import net.mqzon.mapletree.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class Mapletree implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/net/mqzon/mapletree/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package net.mqzon.mapletree.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.mqzon.mapletree.Mapletree;
+
+public class ModBlocks {
+
+    public static final Block MAPLE_PLANKS = registerBlock("maple_planks",
+            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS).requiresTool().));
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(Mapletree.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(Mapletree.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        Mapletree.LOGGER.info("Registering Mod Blocks for " +Mapletree.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.MAPLE_PLANKS);
+        });
+    }
+}
