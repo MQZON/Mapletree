2b706c52267dad31c3e8482f5419146e14365de8
MQZON <mxwllsky@gmail.com>
2024-12-22 16:25:34 -0800

    Switch maple leaves to generated models and blockstates

--- src/main/java/net/mqzon/mapletree/datagen/ModModelProvider.java
+++ src/main/java/net/mqzon/mapletree/datagen/ModModelProvider.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.TexturedModel;
 import net.mqzon.mapletree.block.ModBlocks;
 
 public class ModModelProvider extends FabricModelProvider {
@@ -13,16 +14,15 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        BlockStateModelGenerator.BlockTexturePool maplePool =
-                blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.MAPLE_PLANKS);
+        blockStateModelGenerator.registerSingleton(ModBlocks.MAPLE_LEAVES, TexturedModel.LEAVES);
 
         blockStateModelGenerator.registerLog(ModBlocks.MAPLE_LOG)
                 .log(ModBlocks.MAPLE_LOG).wood(ModBlocks.MAPLE_WOOD);
         blockStateModelGenerator.registerLog(ModBlocks.STRIPPED_MAPLE_LOG)
                 .log(ModBlocks.STRIPPED_MAPLE_LOG).wood(ModBlocks.STRIPPED_MAPLE_WOOD);
 
-//        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAPLE_PLANKS);
-
+        BlockStateModelGenerator.BlockTexturePool maplePool =
+                blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.MAPLE_PLANKS);
         maplePool.slab(ModBlocks.MAPLE_SLAB);
         maplePool.stairs(ModBlocks.MAPLE_STAIRS);
 
