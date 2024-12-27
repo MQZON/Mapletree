9c8b7f1af425795dabfa4197bd6c5e536f84f342
MQZON <mxwllsky@gmail.com>
2024-12-27 15:57:06 -0800

    Add map colors

--- src/main/java/net/mqzon/mapletree/block/ModBlocks.java
+++ src/main/java/net/mqzon/mapletree/block/ModBlocks.java
@@ -22,9 +22,11 @@ import net.mqzon.mapletree.world.tree.ModSaplingGenerators;
 public class ModBlocks {
 
     public static final Block MAPLE_SAPLING = registerBlock("maple_sapling",
-            new SaplingBlock(ModSaplingGenerators.MAPLE, AbstractBlock.Settings.copy(Blocks.OAK_SAPLING)));
+            new SaplingBlock(ModSaplingGenerators.MAPLE, AbstractBlock.Settings.copy(Blocks.OAK_SAPLING)
+                    .mapColor(MapColor.ORANGE)));
     public static final Block RED_MAPLE_SAPLING = registerBlock("red_maple_sapling",
-            new SaplingBlock(ModSaplingGenerators.RED_MAPLE, AbstractBlock.Settings.copy(Blocks.OAK_SAPLING)));
+            new SaplingBlock(ModSaplingGenerators.RED_MAPLE, AbstractBlock.Settings.copy(Blocks.OAK_SAPLING)
+                    .mapColor(MapColor.RED)));
 
     public static final Block POTTED_MAPLE_SAPLING = registerBlockWithoutBlockItem("potted_maple_sapling",
             new FlowerPotBlock(MAPLE_SAPLING, AbstractBlock.Settings.copy(Blocks.POTTED_OAK_SAPLING)));
@@ -32,54 +34,65 @@ public class ModBlocks {
             new FlowerPotBlock(RED_MAPLE_SAPLING, AbstractBlock.Settings.copy(Blocks.POTTED_OAK_SAPLING)));
 
     public static final Block MAPLE_LEAVES = registerBlock("maple_leaves",
-            new MapleLeavesBlock(AbstractBlock.Settings.copy(Blocks.OAK_LEAVES), ModParticles.MAPLE_LEAVES_PARTICLE));
+            new MapleLeavesBlock(AbstractBlock.Settings.copy(Blocks.OAK_LEAVES).mapColor(MapColor.ORANGE),
+                    ModParticles.MAPLE_LEAVES_PARTICLE));
     public static final Block RED_MAPLE_LEAVES = registerBlock("red_maple_leaves",
-            new MapleLeavesBlock(AbstractBlock.Settings.copy(Blocks.OAK_LEAVES), ModParticles.RED_MAPLE_LEAVES_PARTICLE));
+            new MapleLeavesBlock(AbstractBlock.Settings.copy(Blocks.OAK_LEAVES).mapColor(MapColor.RED),
+                    ModParticles.RED_MAPLE_LEAVES_PARTICLE));
 
     public static final Block MAPLE_LOG = registerBlock("maple_log",
-            new PillarBlock(AbstractBlock.Settings.copy(Blocks.OAK_LOG).requiresTool()));
+            new PillarBlock(AbstractBlock.Settings.copy(Blocks.OAK_LOG).mapColor(MapColor.OAK_TAN).requiresTool()));
     public static final Block MAPLE_WOOD = registerBlock("maple_wood",
-            new PillarBlock(AbstractBlock.Settings.copy(Blocks.OAK_WOOD).requiresTool()));
+            new PillarBlock(AbstractBlock.Settings.copy(Blocks.OAK_WOOD).mapColor(MapColor.OAK_TAN).requiresTool()));
     public static final Block STRIPPED_MAPLE_LOG = registerBlock("stripped_maple_log",
-            new PillarBlock(AbstractBlock.Settings.copy(Blocks.STRIPPED_OAK_LOG).requiresTool()));
+            new PillarBlock(AbstractBlock.Settings.copy(Blocks.STRIPPED_OAK_LOG).mapColor(MapColor.OAK_TAN).requiresTool()));
     public static final Block STRIPPED_MAPLE_WOOD = registerBlock("stripped_maple_wood",
-            new PillarBlock(AbstractBlock.Settings.copy(Blocks.STRIPPED_OAK_WOOD).requiresTool()));
+            new PillarBlock(AbstractBlock.Settings.copy(Blocks.STRIPPED_OAK_WOOD).mapColor(MapColor.OAK_TAN).requiresTool()));
 
     public static final Block MAPLE_PLANKS = registerBlock("maple_planks",
-            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS).requiresTool()));
+            new Block(AbstractBlock.Settings.copy(Blocks.OAK_PLANKS)
+                    .mapColor(MapColor.TERRACOTTA_WHITE).requiresTool()));
 
     public static final Block MAPLE_SLAB = registerBlock("maple_slab",
-            new SlabBlock(AbstractBlock.Settings.copy(Blocks.OAK_SLAB)));
+            new SlabBlock(AbstractBlock.Settings.copy(Blocks.OAK_SLAB).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_STAIRS = registerBlock("maple_stairs",
-            new StairsBlock(MAPLE_PLANKS.getDefaultState(), AbstractBlock.Settings.copy(Blocks.OAK_STAIRS)));
+            new StairsBlock(MAPLE_PLANKS.getDefaultState(),
+                    AbstractBlock.Settings.copy(Blocks.OAK_STAIRS).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_FENCE = registerBlock("maple_fence",
-            new FenceBlock(AbstractBlock.Settings.copy(Blocks.OAK_FENCE)));
+            new FenceBlock(AbstractBlock.Settings.copy(Blocks.OAK_FENCE).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_FENCE_GATE = registerBlock("maple_fence_gate",
-            new FenceGateBlock(WoodType.OAK, AbstractBlock.Settings.copy(Blocks.OAK_FENCE_GATE)));
+            new FenceGateBlock(WoodType.OAK,
+                    AbstractBlock.Settings.copy(Blocks.OAK_FENCE_GATE).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_DOOR = registerBlock("maple_door",
-            new DoorBlock(WoodType.OAK.setType(), AbstractBlock.Settings.copy(Blocks.OAK_DOOR)));
+            new DoorBlock(WoodType.OAK.setType(),
+                    AbstractBlock.Settings.copy(Blocks.OAK_DOOR).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_TRAPDOOR = registerBlock("maple_trapdoor",
-            new TrapdoorBlock(WoodType.OAK.setType(),AbstractBlock.Settings.copy(Blocks.OAK_TRAPDOOR)));
+            new TrapdoorBlock(WoodType.OAK.setType(), AbstractBlock.Settings.copy(Blocks.OAK_TRAPDOOR)
+                    .mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_PRESSURE_PLATE = registerBlock("maple_pressure_plate",
-            new PressurePlateBlock(WoodType.OAK.setType(), AbstractBlock.Settings.copy(Blocks.OAK_TRAPDOOR)));
+            new PressurePlateBlock(WoodType.OAK.setType(), AbstractBlock.Settings.copy(Blocks.OAK_TRAPDOOR)
+                    .mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_BUTTON = registerBlock("maple_button",
-            new ButtonBlock(WoodType.OAK.setType(), 15, AbstractBlock.Settings.copy(Blocks.OAK_BUTTON)));
+            new ButtonBlock(WoodType.OAK.setType(), 15, AbstractBlock.Settings.copy(Blocks.OAK_BUTTON)
+                    .mapColor(MapColor.TERRACOTTA_WHITE)));
 
     public static final Identifier MAPLE_SIGN_TEXTURE = Identifier.of(Mapletree.MOD_ID, "entity/signs/maple");
     public static final Identifier MAPLE_HANGING_SIGN_TEXTURE = Identifier.of(Mapletree.MOD_ID, "entity/signs/hanging/maple");
     public static final Identifier MAPLE_HANGING_GUI_SIGN_TEXTURE = Identifier.of(Mapletree.MOD_ID, "textures/gui/hanging_signs/maple");
 
     public static final Block MAPLE_STANDING_SIGN = registerBlock("maple_standing_sign",
-            new TerraformSignBlock(MAPLE_SIGN_TEXTURE, AbstractBlock.Settings.copy(Blocks.OAK_SIGN)));
+            new TerraformSignBlock(MAPLE_SIGN_TEXTURE, AbstractBlock.Settings.copy(Blocks.OAK_SIGN)
+                    .mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_WALL_SIGN = registerBlock("maple_wall_sign",
             new TerraformWallSignBlock(MAPLE_SIGN_TEXTURE, AbstractBlock.Settings.copy(Blocks.OAK_WALL_SIGN)
-                    .dropsLike(MAPLE_STANDING_SIGN)));
+                    .mapColor(MapColor.TERRACOTTA_WHITE).dropsLike(MAPLE_STANDING_SIGN)));
     public static final Block MAPLE_HANGING_SIGN = registerBlock("maple_hanging_sign",
             new TerraformHangingSignBlock(MAPLE_HANGING_SIGN_TEXTURE, MAPLE_HANGING_GUI_SIGN_TEXTURE,
-                    AbstractBlock.Settings.copy(Blocks.OAK_HANGING_SIGN)));
+                    AbstractBlock.Settings.copy(Blocks.OAK_HANGING_SIGN).mapColor(MapColor.TERRACOTTA_WHITE)));
     public static final Block MAPLE_WALL_HANGING_SIGN = registerBlock("maple_wall_hanging_sign",
             new TerraformWallHangingSignBlock(MAPLE_HANGING_SIGN_TEXTURE, MAPLE_HANGING_GUI_SIGN_TEXTURE,
-                    AbstractBlock.Settings.copy(Blocks.OAK_WALL_HANGING_SIGN).dropsLike(MAPLE_HANGING_SIGN)));
+                    AbstractBlock.Settings.copy(Blocks.OAK_WALL_HANGING_SIGN)
+                            .mapColor(MapColor.TERRACOTTA_WHITE).dropsLike(MAPLE_HANGING_SIGN)));
 
     public static final BlockFamily MAPLE_FAMILY =BlockFamilies.register(MAPLE_PLANKS)
             .sign(MAPLE_STANDING_SIGN, MAPLE_WALL_SIGN)
