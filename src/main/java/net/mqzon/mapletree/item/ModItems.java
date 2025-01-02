56196e657b5db99f9c2911161f6ef7a9ee2445ec
MQZON <mxwllsky@gmail.com>
2025-01-01 17:19:14 -0800

    Add bottle recipe remainder to syrup bottle

--- src/main/java/net/mqzon/mapletree/item/ModItems.java
+++ src/main/java/net/mqzon/mapletree/item/ModItems.java
@@ -2,10 +2,7 @@ package net.mqzon.mapletree.item;
 
 import com.terraformersmc.terraform.boat.api.item.TerraformBoatItemHelper;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.HangingSignItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.SignItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -25,7 +22,8 @@ public class ModItems {
 
     public static Item SAP_BUCKET = registerItem("sap_bucket", new Item(new Item.Settings().maxCount(1)));
 
-    public static final Item SYRUP_BOTTLE = registerItem("syrup_bottle", new ModDrinkItem(new Item.Settings().food(ModFoodComponents.SYRUP_BOTTLE).maxCount(16)));
+    public static final Item SYRUP_BOTTLE = registerItem("syrup_bottle", new ModDrinkItem
+            (new Item.Settings().food(ModFoodComponents.SYRUP_BOTTLE).maxCount(16).recipeRemainder(Items.GLASS_BOTTLE)));
     public static final Item GLAZED_CARROTS = registerItem("glazed_carrots", new Item(new Item.Settings().food(ModFoodComponents.GLAZED_CARROTS)));
     public static final Item GLAZED_SALMON = registerItem("glazed_salmon", new Item(new Item.Settings().food(ModFoodComponents.GLAZED_SALMON)));
     public static final Item PANCAKES = registerItem("pancakes", new Item(new Item.Settings().food(ModFoodComponents.PANCAKES)));
