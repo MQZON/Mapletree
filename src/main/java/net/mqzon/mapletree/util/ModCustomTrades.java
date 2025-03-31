9c1fdd015bc03b5a44473998cadeb891c79ab32c
MQZON <mxwllsky@gmail.com>
2025-03-30 20:04:40 -0700

    Fix sapling trades

--- src/main/java/net/mqzon/mapletree/util/ModCustomTrades.java
+++ src/main/java/net/mqzon/mapletree/util/ModCustomTrades.java
@@ -3,22 +3,28 @@ package net.mqzon.mapletree.util;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
 import net.minecraft.village.TradedItem;
 import net.mqzon.mapletree.block.ModBlocks;
 
 public class ModCustomTrades {
     public static void registerCustomTrades() {
-        TradeOfferHelper.registerWanderingTraderOffers(2,
-                factories -> {
-                    factories.add((entity, random) -> new TradeOffer(
-                            new TradedItem(Items.EMERALD, 5),
-                            new ItemStack(ModBlocks.MAPLE_SAPLING, 1), 8, 1, 1
-                    ));
-                    factories.add((entity, random) -> new TradeOffer(
-                            new TradedItem(Items.EMERALD, 5),
-                            new ItemStack(ModBlocks.RED_MAPLE_SAPLING, 1), 8, 1, 1
-                    ));
-                });
+        TradeOffer MapleSaplingOffer = new TradeOffer(
+                new TradedItem(Items.EMERALD, 5),
+                new ItemStack(ModBlocks.MAPLE_SAPLING, 1), 8, 1, 1
+        );
+        TradeOffer RedMapleSaplingOffer = new TradeOffer(
+                new TradedItem(Items.EMERALD, 5),
+                new ItemStack(ModBlocks.RED_MAPLE_SAPLING, 1), 8, 1, 1
+        );
+        Identifier specialItems = TradeOfferHelper.WanderingTraderOffersBuilder.SELL_SPECIAL_ITEMS_POOL;
+        TradeOffers.Factory saplingFactory = (entity, random) -> MapleSaplingOffer;
+        TradeOffers.Factory redSaplingFactory = (entity, random) -> RedMapleSaplingOffer;
+
+        TradeOfferHelper.registerWanderingTraderOffers(( builder ) -> builder.addOffersToPool(specialItems, saplingFactory));
+        TradeOfferHelper.registerWanderingTraderOffers(( builder ) -> builder.addOffersToPool(specialItems, redSaplingFactory));
+
     }
 }
